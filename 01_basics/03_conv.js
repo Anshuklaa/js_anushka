7f64753d0accf030aeefb78ec6bc64c687a35949
Anshuklaa <143292903+Anshuklaa@users.noreply.github.com>
2024-07-06 18:37:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_conv.js
@@ -0,0 +1,36 @@
+let score="anushka"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+//"33"= 33
+//"33abc"=NaN
+//true = 1
+//false = 0
+
+//boolean to number
+let isloggedin = true
+console.log(typeof isloggedin);
+let valueInNumm = Number(isloggedin)
+console.log(typeof valueInNumm);
+console.log(valueInNumm);
+//boolean to string
+let valueInNummm = String(isloggedin)
+console.log(typeof valueInNummm);
+console.log(valueInNummm);
+//string to boolean\
+let strrr=""
+let valueInNummmm = Boolean(strrr)
+console.log(typeof valueInNummmm);
+console.log(valueInNummmm);
+// number to boolean
+let strr=0
+let valueInNu = Boolean(strr)
+console.log(typeof valueInNu);
+console.log(valueInNu);
+// number to string
+let str=23
+let valueIn = String(str)
+console.log(typeof valueIn);
+console.log(valueIn);
