c76687e8e210b5c63ea1f57c9938a6692300c99a
Anshuklaa <143292903+Anshuklaa@users.noreply.github.com>
2024-07-08 18:38:52 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,38 @@
+// // // primitive and non primitive DataT 
+// // //7: string,number,null, boolean  undefined , symbol - unique , BigInt
+// // // reference type , non primitive , arrays, objects, function, 
+// // // const id=symbol('123')
+// // // const anotherid= Symbol('123')
+// // // console.log(id=== anotherid);
+// // //const bignumber = 7537635735767635n 
+
+// // // console.log(typeof bignumber);
+// // // let obj={
+// // //     name:"hetsh"
+// // //     age: 22,
+// // // }
+// // // const func = functio(){
+// // //     console.log("hello");
+// // // }
+// // let outsidetemp=null
+// // console.log(typeof outsidetemp);
+// // Primitive datatypes:
+// // String - string
+// // Number - number
+// // Boolean - Boolean
+// // Null - object(****)
+// // Undefined - undefined
+// // BigInt - bigint
+
+// // Non-primitive datatypes:
+// // Array - object
+// // Function - function(object)
+// // Object - object
+
+// //----------------------
+// stack-prim and heap-nonprim - Ref
+
+// let namee
+// let mee="namee"
+// console.log(mee);
+
